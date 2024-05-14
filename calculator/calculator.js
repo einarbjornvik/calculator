e471993a6c337696ca0e863bd883361370351f9e
einarbjornvik <162742510+einarbjornvik@users.noreply.github.com>
2024-05-14 12:35:19 +0200

    Add files via upload

---
+++ calculator/calculator.js
@@ -0,0 +1,10 @@
+let tall1 = prompt("skriv et tall");
+let tall2 = prompt("skriv et tall til");
+
+function ganger(tall1, tall2)
+{
+    return tall1 * tall2;
+}
+
+let svar = ganger(tall1, tall2);
+console.log(svar);
